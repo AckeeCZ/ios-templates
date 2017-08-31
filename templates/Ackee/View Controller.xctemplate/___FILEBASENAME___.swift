7e4adc747633fbb00a10f874ebbd0500b16e6b05
Jan Misar <misar.jan@gmail.com>
2017-08-31 11:11:16 +0200

    Revert "changed the ordering of methods - setupBindings is more important in ViewController and as such should be higher in the code than View Life Cycle methods"
    This reverts commit e9f5afba644edcffe7304cd2429993a3d1345b3f.

--- templates/Ackee/View Controller.xctemplate/___FILEBASENAME___.swift
+++ templates/Ackee/View Controller.xctemplate/___FILEBASENAME___.swift
@@ -9,32 +9,41 @@
 import UIKit
 import ReactiveSwift
 
-final class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_controllerSubclass___ {
-    
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_controllerSubclass___ {
+
     // MARK: Dependencies
+
     let viewModel: ___VARIABLE_viewModel___
-    
+
     // MARK: Initializers
+
     required init(viewModel: ___VARIABLE_viewModel___) {
         self.viewModel = viewModel
+
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // MARK: Bindings
-    private func setupBindings() {
-    }
-    
-    // MARK: View Life Cycle
+
+    // MARK: View life cycle
+
     override func loadView() {
         super.loadView()
+
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         setupBindings()
     }
+
+    // MARK: Helpers
+
+    private func setupBindings() {
+
+    }
+
 }

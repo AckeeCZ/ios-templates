9596a55ae89c62377b457b493d749fa4f7dc2f6e
Martin Žid <martin.zid@ackee.cz>
2018-04-27 17:07:04 +0200

    add actions for VM

--- templates/Ackee/ViewController+ViewModel.xctemplate/ViewModel/___FILEBASENAME___ViewModel.swift
+++ templates/Ackee/ViewController+ViewModel.xctemplate/ViewModel/___FILEBASENAME___ViewModel.swift
@@ -9,7 +9,11 @@
 import ReactiveSwift
 
 protocol ___FILEBASENAMEASIDENTIFIER___ing {
+	var actions: ___VARIABLE_productName___Actions { get }
+}
 
+extension ___FILEBASENAMEASIDENTIFIER___ing where Self: ___VARIABLE_productName___Actions {
+    var actions: ___VARIABLE_productName___Actions { return self }
 }
 
 class ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___ing {

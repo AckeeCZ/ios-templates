06a9cc93d56fe024ace685685f29a81f0f237230
Martin Žid <martin.zid@ackee.cz>
2018-04-27 16:08:27 +0200

    add template for VC and VM

---
+++ templates/Ackee/ViewController+ViewModel.xctemplate/View/ViewController/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+import ReactiveSwift
+
+final class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_controllerSubclass___ {
+
+    // MARK: Dependencies
+
+    let viewModel: ___VARIABLE_productName___ViewModeling
+
+    // MARK: Initializers
+
+    required init(viewModel: ___VARIABLE_productName___ViewModeling) {
+        self.viewModel = viewModel
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: View life cycle
+
+    override func loadView() {
+        super.loadView()
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupBindings()
+    }
+
+    // MARK: Helpers
+
+    private func setupBindings() {
+
+    }
+
+}
